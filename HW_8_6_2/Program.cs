71f4b904a33ff4d3d757673e68917140f73a0423
Repnikov.A <repnikov.a@florcom.ru>
2023-08-11 15:59:17 +0300

    На проверку

--- HW_8_6_2/Program.cs
+++ HW_8_6_2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+
+namespace HW_8_6_2;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к каталогу");
+        string path = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Размер вложенный файлов и папок {DirectorySize.Size(path)} байт");
+
+    }
+}
